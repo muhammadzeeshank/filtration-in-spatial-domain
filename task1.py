a808cb431745bcf02ab8ae63c872ad0d62a9eaa0
muhammadzeeshank <zeeshan124go@gmail.com>
2021-04-24 09:29:33 +0500

    improved

--- task1.py
+++ task1.py
@@ -12,18 +12,24 @@ while(True):
 value = float(input("Enter maske value: "))
 mask = np.full([size, size], value)
 # Reading image from drive
-img1 = cv2.imread('images/img_face.tif', 0)
+# img1 = cv2.imread('images/img_face.tif', 0)
+img1 = np.full([size, size], value)
 # Padding image
 padsize = int(size/2)
 img = np.lib.pad(img1, ((padsize, padsize), (padsize, padsize)), mode='constant',
                  constant_values=(0, 0))
-rows, cols = img.shape
-for i in range(size):
-    for j in range(size):
-        val += img[i][j] * mask[i][j]
+rows, cols = img1.shape
+for y in range(rows):
+    for x in range(cols):
+        val = 0
+        for i in range(size):
+            for j in range(size):
+                val += img[i + y][j + x] * mask[i][j]
+        img1[y][x] = val
 
 print(mask)
-
-cv2.imshow('img', img)
+print(img)
+print(img1)
+# cv2.imshow('img', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
