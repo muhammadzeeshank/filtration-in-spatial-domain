e183ac57d4c5db52455af87c63419c06097f2904
muhammadzeeshank <zeeshan124go@gmail.com>
2021-04-25 10:52:02 +0500

    comments added

--- task1.py
+++ task1.py
@@ -83,8 +83,11 @@ def medianFilter(img, img1, size):
 orig_img = cv2.imread('images/img2a.tif', 0)
 mask, mask_size = constructMask()
 pad_img = padImage(orig_img, mask_size)
+# # to apply filter by your choice
 # final_img = applyFilter(pad_img, orig_img, mask)
+# # to apply median Filter
 final_img = medianFilter(pad_img, orig_img, mask_size)
+# final image
 cv2.imwrite('img1b.jpg', final_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
